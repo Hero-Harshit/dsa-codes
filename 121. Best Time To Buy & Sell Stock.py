6f83258454c544bc4c73fd458f03d12c5e1ef1dd
Harshit <heroharshitlaptop@gmail.com>
2026-06-14 02:18:52 +0530

    121 Optimized Solution

--- 121. Best Time To Buy & Sell Stock.py
+++ 121. Best Time To Buy & Sell Stock.py
@@ -9,4 +9,24 @@ class Solution:
                 if (prices[j] - prices[i]) > max_profit:
                     max_profit = prices[j] - prices[i]
 
-        return max_profit            
+        return max_profit
+    
+Optimised Solution:    
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
